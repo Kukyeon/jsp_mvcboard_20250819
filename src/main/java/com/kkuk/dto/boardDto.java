2252cde3c6f45ae15da3e5c70a585319a91623dc
Kukyeon <rnrdus2000@naver.com>
2025-08-19 11:39:48 +0900

    게시판 DTO

---
+++ src/main/java/com/kkuk/dto/boardDto.java
@@ -0,0 +1,79 @@
+package com.kkuk.dto;
+
+public class boardDto {
+
+	private int bnum; // 글 순서
+	private String btitle; // 글 제목
+	private String bcontent; // 글 내용
+	private String memberid; // 글쓴이
+	private int bhit; // 조회수
+	private String bdate; // 글쓴날자
+	
+	public boardDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public boardDto(int bnum, String btitle, String bcontent, String memberid, int bhit, String bdate) {
+		super();
+		this.bnum = bnum;
+		this.btitle = btitle;
+		this.bcontent = bcontent;
+		this.memberid = memberid;
+		this.bhit = bhit;
+		this.bdate = bdate;
+	}
+
+	public int getBnum() {
+		return bnum;
+	}
+
+	public void setBnum(int bnum) {
+		this.bnum = bnum;
+	}
+
+	public String getBtitle() {
+		return btitle;
+	}
+
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+
+	public String getBcontent() {
+		return bcontent;
+	}
+
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+
+	public String getMemberid() {
+		return memberid;
+	}
+
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+
+	public int getBhit() {
+		return bhit;
+	}
+
+	public void setBhit(int bhit) {
+		this.bhit = bhit;
+	}
+
+	public String getBdate() {
+		return bdate;
+	}
+
+	public void setBdate(String bdate) {
+		this.bdate = bdate;
+	}
+	
+	
+	
+	
+	
+}
